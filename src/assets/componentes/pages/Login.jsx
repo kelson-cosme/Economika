d95c2a7f82c590b978fedcfb59f70e9d291a2968
kelson-cosme <kelson.almeida123@gmail.com>
2024-10-26 21:46:33 -0400

    login

--- src/assets/componentes/pages/Login.jsx
+++ src/assets/componentes/pages/Login.jsx
@@ -1,22 +1,58 @@
 import { useEffect, useState } from "react"
 
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH,
+    projectId: import.meta.env.VITE_ID,
+    storageBucket: import.meta.env.VITE_STORAGE,
+  };
+
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+
 function Login(){
 
-    const [cpf, setCpf] = useState("")
+    const [email, setEmail] = useState("")
     const [senha, setSenha] = useState("")
-
-    function enviar(){
-        console.log(cpf, senha)
+  
+     // Entrar
+  const handleLogin = async (event) => {
+    event.preventDefault();  
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, senha);
+      const user = userCredential.user;
+      console.log("mimimii")
+    } catch (error) {
+        console.log(error)
     }
+  };
+
+
+    // Sair
+    function sair(){
+        signOut(auth).then(() => {
+            console.log("Sign-out successful")
+            // Sign-out successful.
+          }).catch((error) => {
+            // An error happened.
+          });
+    }
+    
+
 
     return(
         <>
-            <form >
-                <input type="cpf" onChange={(e) => setCpf(e.target.value)} />
-                <input type="password" onChange={(e) => setSenha(e.target.value)}/>
+            <form onSubmit={handleLogin}>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)}/>
+
+            <button type="submit">ENVIAR</button>
+
             </form>
 
-            <button onClick={enviar}>ENVIAR</button>
         </>
     )
 }

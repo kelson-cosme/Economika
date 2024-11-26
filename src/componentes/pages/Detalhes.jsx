ce6dcaf8a0f7a51446875c086ceed21ba8042e1d
kelson.almeida <kelson.almeida123@gmail.com>
2024-11-26 17:15:06 -0400

    pagina detalhes estilizada

--- src/componentes/pages/Detalhes.jsx
+++ src/componentes/pages/Detalhes.jsx
@@ -2,8 +2,11 @@ import { useParams } from "react-router-dom";
 import Navbar from "../navBar/Navbar";
 import ProdutosContext from '../contextProdutos/ProdutosContext';
 import { useContext, useState, useEffect } from "react";
+import UserContext from "./Context";
+import "./Detalhes.css"
 
 function Detalhes() {
+    const { adCarrinho } = useContext(UserContext); // Consumir o contexto corretamente
     const { produtos } = useContext(ProdutosContext);
     const { id } = useParams(); // Extraindo o parâmetro `id` corretamente
     const [produtosFilter, setProdutosFilter] = useState(null);
@@ -22,10 +25,16 @@ function Detalhes() {
 
 
             {produtosFilter ? (
-                <div>
-                    <h1>{produtosFilter.titulo}</h1>
-                    <p>Preço: R$ {produtosFilter.preco}</p>
-                    <img src={produtosFilter.imagem} alt={produtosFilter.titulo} />
+                <div className="produtoDetalhes">
+                    <div className="produtoLeft">
+                        <h2>{produtosFilter.titulo}</h2>
+                        <img src={produtosFilter.imagem} alt={produtosFilter.titulo} />
+                    </div>
+                    
+                    <div className="produtoRight">
+                        <h2>R$ {produtosFilter.preco}</h2>
+                        <button onClick={() => adCarrinho(produtosFilter)}>Adicionar ao Carrinho</button>
+                    </div>
                     {/* Outras informações do produto */}
                 </div>
             ) : (
